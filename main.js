85045944f96e388581adca15f994b135f4690f73
unknown <jithinbp007@gmail.com>
2024-04-04 14:26:50 +0530

    updating the functionality for price change

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 import "./sass/style.scss";
+
+const radio = document.querySelectorAll("[name='pricing']");
+const basicPriceElement = document.querySelector("#basic-price");
+const professionalPriceElement = document.querySelector("#professional-price");
+const masterPriceElement = document.querySelector("#master-price");
+const anuallyChecked = document.querySelector("[role='switch']:checked");
+
+function monthlyPricing() {
+  basicPriceElement.textContent = "19.99";
+  professionalPriceElement.textContent = "24.99";
+  masterPriceElement.textContent = "39.99";
+}
+
+function annualPricing() {
+  basicPriceElement.textContent = "199.9";
+  professionalPriceElement.textContent = "249.9";
+  masterPriceElement.textContent = "399.9";
+}
+
+if (anuallyChecked) {
+  annualPricing();
+}
+
+function handleOnChange(e) {
+  console.log(document.querySelector("[role='switch']:checked"));
+
+  if (e.target.id === "monthly") {
+    monthlyPricing();
+  } else if (e.target.id === "annually") {
+    annualPricing();
+  }
+}
+radio.forEach((btn) => btn.addEventListener("change", handleOnChange));
